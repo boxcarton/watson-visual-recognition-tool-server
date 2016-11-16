9f9ca49ec842493b7281ffbfc7b72fe0419e8f1f
Josh Zheng <denial@gmail.com>
2016-11-15 18:33:34 -0800

    allowing crossorigin

--- watson_visual_recognition_tool/controllers.py
+++ watson_visual_recognition_tool/controllers.py
@@ -1,5 +1,7 @@
 import os
 from tempfile import TemporaryFile
+from datetime import timedelta
+from functools import update_wrapper
 
 from flask import Flask, request, Response, g
 from flask import render_template, url_for, send_from_directory
@@ -12,21 +14,67 @@ from watson_visual_recognition_tool import app
 
 my_vr = WatsonVisualRecognition()
 
-@app.route('/api/classifiers', methods=['GET'])
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+@app.route('/api/classifiers', methods=['GET','OPTIONS'])
+@crossdomain(origin='*')
 def get_custom_classifiers():
   api_key = request.args.get('apiKey')
   classifiers = my_vr.list_classifiers(api_key)
   response = jsonify(classifiers)
   return response, response.status_code
 
-@app.route('/api/classifier/<id>', methods=['GET'])
+@app.route('/api/classifier/<id>', methods=['GET','OPTIONS'])
+@crossdomain(origin='*')
 def get_custom_classifier_detail(id):
   api_key = request.args.get('apiKey')
   classifier = my_vr.get_classifier(id, api_key)
   response = jsonify(classifier)
   return response, response.status_code
 
-@app.route('/api/classifiers', methods=['POST'])
+@app.route('/api/classifiers', methods=['POST','OPTIONS'])
+@crossdomain(origin='*')
 def create_custom_classifier():
   classifier_name = request.form['classifier_name']
   api_key = request.form['api_key']
@@ -48,7 +96,8 @@ def create_custom_classifier():
   
   return response, response.status_code
 
-@app.route('/api/classify', methods=['POST'])
+@app.route('/api/classify', methods=['POST','OPTIONS'])
+@crossdomain(origin='*')
 def classify_image():
   classifier_id = request.form['classifier_id']
   api_key = request.form['api_key']
@@ -73,7 +122,8 @@ def classify_image():
   
   return response, response.status_code
 
-@app.route('/api/classifier/<id>', methods=['DELETE'])
+@app.route('/api/classifier/<id>', methods=['DELETE','OPTIONS'])
+@crossdomain(origin='*')
 def delete_custom_classifier(id):
   api_key = request.headers.get('apiKey')
   response = my_vr.delete_classifier(id, api_key)
@@ -82,11 +132,13 @@ def delete_custom_classifier(id):
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
+@crossdomain(origin='*')
 def index(**kwargs):
   return make_response(open('watson_visual_recognition_tool/templates/index.html').read())
 
 # special file handlers and error handlers
 @app.route('/favicon.ico')
+@crossdomain(origin='*')
 def favicon():
   return send_from_directory(os.path.join(app.root_path, 'static'),
          'img/favicon.ico')
